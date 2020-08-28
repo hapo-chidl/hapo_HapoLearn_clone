7f758a1dd683ee91a4341260602b28359b9289e6
linhchi2406 <linhchi24060508@gmail.com>
2020-08-28 13:38:51 +0700

    Add file

---
+++ resources/views/layouts/course.blade.php
@@ -0,0 +1,69 @@
+<section class="course container">
+    <div class="card-deck backend-course row flex-sm-nowrap ml-5 ml-md-0">
+        <div class="card">
+            <div class="image html-course">
+                <img alt=".." class="img-fluid mt-xl-3 pt-3 px-md-5 py-md-5 mt-md-0" src="{{ asset('storage/image/html_css.png') }}">
+            </div>
+            <div class="card-body">
+                <p class="card-title mt-1 mt-md-0">HTML/CSS/js Tutorial</p>
+                <p class="card-text mb-xl-4 mb-md-0">I knew hardly anything about HTML, JS, and CSS before entering New Media. I had coded quite a bit, but never touched
+                    anything in regards to web development.</p>
+                <a class="ml-xl-5 ml-md-4" href="#">Take This Course</a>
+            </div>
+        </div>
+        <div class="card">
+            <div class="image laravel-course d-flex justify-content-center align-items-center">
+                <img alt=".." class="img-fluid px-md-5 py-md-5" src="{{ asset('storage/image/laravel-1-logo-black-and-white.png') }}"></div>
+            <div class="card-body">
+                <p class="card-title mt-1 mt-md-0">LARAVEL Tutorial</p>
+                <p class="card-text mb-xl-4 mb-md-0">I knew hardly anything about HTML, JS, and CSS before entering New Media. I had coded quite a bit, but never touched
+                    anything in regards to web development.</p>
+                <a class="ml-xl-5 ml-md-4" href="#">Take This Course</a>
+        </div>
+        </div>
+        <div class="card">
+            <div class="image php-course"><img alt=".." class="img-fluid mt-md-0 mt-xl-3 mt-sm-3 px-md-5 py-md-5" src="{{ asset('storage/image/php.png') }}"></div>
+            <div class="card-body">
+                <p class="card-title mt-1 mt-md-0">PHP Tutorial</p>
+                <p class="card-text mb-xl-4 mb-md-0">I knew hardly anything about HTML, JS, and CSS before entering New Media. I had coded quite a bit, but never touched
+                anything in regards to web development.</p>
+                <a class="ml-xl-5 ml-md-4" href="#">Take This Course</a>
+            </div>
+        </div>
+    </div>
+    <div class="other-course">
+        <p>Other Courses</p>
+    </div>
+    <div class="card-deck frontend-course row flex-sm-nowrap ml-5 ml-md-0">
+        <div class="card">
+            <div class="image css-course"><img alt=".." class="img-fluid mt-4 mt-md-3 px-md-5 py-md-5" src="{{ storage('storage/image/CSS.png') }}"></div>
+            <div class="card-body">
+                <p class="card-title mt-1 mt-md-0">CSS Tutorial</p>
+                <p class="card-text mb-xl-4 mb-md-0">I knew hardly anything about HTML, JS, and CSS before entering New Media. I had coded quite a bit, but never touched
+                anything in regards to web development.</p>
+                <a class="ml-xl-5 ml-md-4" href="#">Take This Course</a>
+            </div>
+        </div>
+        <div class="card">
+            <div class="image ruby-course"><img alt=".." class="img-fluid mt-3 px-md-5 py-md-5 mt-md-1" src="{{ asset('storage/image/Ruby.png') }}"></div>
+            <div class="card-body">
+                <p class="card-title mt-1 mt-md-0">Ruby on rails Tutorial</p>
+                <p class="card-text mb-xl-4 mb-md-0">I knew hardly anything about HTML, JS, and CSS before entering New Media. I had coded quite a bit, but never touched
+                anything in regards to web development.</p>
+                <a class="ml-xl-5 ml-md-4" href="#">Take This Course</a>
+            </div>
+        </div>
+        <div class="card">
+            <div class="image java-course"><img alt=".." class="img-fluid px-md-5 py-md-5" src="{{ asset('storage/image/java.png') }}"></div>
+            <div class="card-body">
+                <p class="card-title mt-1 mt-md-0">Java Tutorial</p>
+                <p class="card-text mb-xl-4 mb-md-0">I knew hardly anything about HTML, JS, and CSS before entering New Media. I had coded quite a bit, but never touched
+                anything in regards to web development.</p>
+                <a class="ml-xl-5 ml-md-4" href="#">Take This Course</a>
+            </div>
+        </div>
+    </div>
+    <div class="all-of-course mt-4">
+        <p><a href="#">View All Our Courses <img src="{{ asset('storage/image/mui_ten.png') }}"></a></p>
+    </div>
+</section>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,51 @@
+<nav class="navbar navbar-expand-md navbar-light">
+    <div class="container-fluid d-flex flex-xl-nowrap flex-md-wrap">
+        <div class="col-xl-4 col-md-12 col-6 d-md-flex justify-content-md-center justify-content-center">
+            <div class="img-header">
+                <img alt="Hapo_Learn" src="{{ asset('storage/image/Hapo_Learn.png') }}">
+            </div>
+            <button class="navbar-toggler collapsed border-0" data-target="#collapsingNavbar" data-toggle="collapse" id="header-button" type="button">
+                <span class="navbar-toggler-icon"></span>
+                <div class="close-icon py-1">
+                    ✖
+                </div>
+            </button>
+        </div>
+        <div class="col-xl-8 col-md-12 collapse navbar-collapse d-md-flex flex-md-nowrap justify-content-xl-around justify-content-center" id="collapsingNavbar">
+            <ul class="nav navbar-nav menu-header">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">HOME</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">LOGIN/REGISTER</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">LESON&nbsp;DETAIL</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">LIST&nbsp;LESSON</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">ALL&nbsp;COURSE</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">PROFILE</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<section class="header-banner w-100">
+    <div class="header-content">
+        <div class="content">
+            <p class="slogan-up">Learn Anytime, Anywhere</p>
+            <p class="slogan-down">at HapoLearn <img alt="cu" src="{{ asset('storage/image/cu.png') }}"> !</p>
+            <pre>
+                Interactive lessons, "on-the-go"
+                practice, peer support.
+            </pre>
+            <a class="ml-5" href="#">Start Learning Now!</a>
+        </div>
+    </div>
+    <div class="header-footer"></div>
+</section>
