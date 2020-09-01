c50223b508d4e007f0788c72012f2227e4b92ae5
hapo-chidl <68210596+hapo-chidl@users.noreply.github.com>
2020-09-01 16:03:57 +0700

    Update CourseFactory.php

--- database/factories/CourseFactory.php
+++ database/factories/CourseFactory.php
@@ -6,16 +6,10 @@ use App\Model;
 use Faker\Generator as Faker;
 
 $factory->define(App\course\Course::class, function (Faker $faker) {
-    $name = array ("HTML Fundamentals", "PHP Tutorial", "Swift 4 Fundamentals", "Ruby Tutorial", "jQuery Tutorial", 
-                   "Data Science with Python", "CSS Fundamentals", "SQL Fundamentals", "C# Tutorial", "C Tutorial",
-                   "Angular + NetJS", "Machine Learning", "CSS Fundamentals");
+    $name = array ("HTML Fundamentals", "PHP Tutorial", "Swift 4 Fundamentals", "Ruby Tutorial", "jQuery Tutorial", "Data Science with Python", "CSS Fundamentals", "SQL Fundamentals", "C# Tutorial", "C Tutorial", "Angular + NetJS", "Machine Learning", "CSS Fundamentals");
     $image = "storage/image/course.png";
-    $requements = "Vivamus volutpat eros pulvinar velit laoreet, sit amet egestas erat dignissim. Sed quis rutrum tellus, 
-                    sit amet viverra felis. Cras sagittis sem sit amet urna feugiat rutrum. Nam nulla ipsum, venenatis 
-                    malesuada felis quis, ultricies convallis neque. Pellentesque tristique fringilla tempus. Vivamus 
-                    bibendum nibh in dolor pharetra, a euismod nulla dignissim. Aenean viverra tincidunt nibh, in imperdiet nunc.";
-    $description = "Practice during lessons, practice between lessons, practice whenever you can. Master the task, then reinforce 
-                    and test your knowledge with fun, hands-on exercises and interactive quizzes.";
+    $requements = "Vivamus volutpat eros pulvinar velit laoreet, sit amet egestas erat dignissim. Sed quis rutrum tellus, sit amet viverra felis. Cras sagittis sem sit amet urna feugiat rutrum. Nam nulla ipsum, venenatis malesuada felis quis, ultricies convallis neque. Pellentesque tristique fringilla tempus. Vivamus bibendum nibh in dolor pharetra, a euismod nulla dignissim. Aenean viverra tincidunt nibh, in imperdiet nunc.";
+    $description = "Practice during lessons, practice between lessons, practice whenever you can. Master the task, then reinforce and test your knowledge with fun, hands-on exercises and interactive quizzes.";
     
     return [
         'name' => $name[ mt_rand(0, 12)],
