841af89b41df33be7fedff79e3c3cda6d560aec1
hapo-chidl <68210596+hapo-chidl@users.noreply.github.com>
2020-09-01 16:04:33 +0700

    Update UserFactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,9 +21,7 @@ $factory->define(App\course\User::class, function (Faker $faker) {
     $image = "storage\image\account.png";
     $role = mt_rand(0, 1);
     if($role == 1){
-        $introduction = "Vivamus volutpat eros pulvinar velit laoreet, sit amet egestas erat dignissim.
-                         Sed quis rutrum tellus, sit amet viverra felis. Cras sagittis sem sit amet urna
-                         feugiat rutrun. ";
+        $introduction = "Vivamus volutpat eros pulvinar velit laoreet, sit amet egestas erat dignissim. Sed quis rutrum tellus, sit amet viverra felis. Cras sagittis sem sit amet urn feugiat rutrun. ";
     } else $introduction = NULL;
     
     return [
